06802dc7fdeba1963f76280e8b340ca5719ec073
Takayuki Komatsu <komatsutakayuki235@live.com>
2021-08-14 20:09:48 -0300

    reutilizando o componente

--- pages/basicos/usandoTitulo.jsx
+++ pages/basicos/usandoTitulo.jsx
@@ -2,11 +2,15 @@ import Titulo from '../../components/Titulo'
 
 function usandoTitulo() {
 	return (
-		<Titulo
-			principal='Página de Cadastro'
-			secundario='Incluir, alterar e excluir coisas'
-		/>
+		<>
+			<Titulo
+				principal='Página de Cadastro'
+				secundario='Incluir, alterar e excluir coisas'
+			/>
+			<Titulo
+				principal='Página de Login'
+				secundario='Informe o seu email e senha'
+			/>
+		</>
 	)
 }
-
-export default usandoTitulo
