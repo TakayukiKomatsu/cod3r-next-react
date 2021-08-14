e781abdce10482ca40da403aa2990a6138ac1259
Takayuki Komatsu <komatsutakayuki235@live.com>
2021-08-14 20:48:47 -0300

    add integracao2 style

--- pages/css/integracao2.jsx
+++ pages/css/integracao2.jsx
@@ -2,11 +2,11 @@ import styles from './integracao2.module.css'
 
 function integracao() {
 	return (
-		<>
+		<div id={styles.integracao2}>
 			<div className={styles.vermelha}>Texto #1</div>
 			<div className={styles.azul}>Texto #2</div>
 			<div className={styles.branca}>Texto #3</div>
-		</>
+		</div>
 	)
 }
 
