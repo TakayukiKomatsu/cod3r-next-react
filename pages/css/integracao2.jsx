5a69b4230701430900343dcd6a1828a72c1a8c38
Takayuki Komatsu <komatsutakayuki235@live.com>
2021-08-14 20:38:58 -0300

    integracao com css #2

---
+++ pages/css/integracao2.jsx
@@ -0,0 +1,13 @@
+import styles from './integracao2.module.css'
+
+function integracao() {
+	return (
+		<>
+			<div className={styles.vermelha}>Texto #1</div>
+			<div className={styles.azul}>Texto #2</div>
+			<div className={styles.branca}>Texto #3</div>
+		</>
+	)
+}
+
+export default integracao
