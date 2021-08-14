8c4712af29ed417c02ea9779ecd127cd0820b715
Takayuki Komatsu <komatsutakayuki235@live.com>
2021-08-14 18:16:23 -0300

    jsx#2 example

--- pages/basicos/jsx2.js
+++ pages/basicos/jsx2.js
@@ -1,7 +1,11 @@
 export default function jsx2() {
-	return (
+	const conteudo = (
 		<div>
 			<h1>JSX #02</h1>
 		</div>
 	)
+	return conteudo
 }
+
+// HTML , CSS e JS => Tecnologias
+// Componentes => (HTML, JS, CSS)
