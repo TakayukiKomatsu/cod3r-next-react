d613a3ecc6f4c45d60c65eda03a9e3d903cc5bcb
Takayuki Komatsu <komatsutakayuki235@live.com>
2021-08-14 19:32:09 -0300

    criando o primeiro componente

---
+++ components/Titulo.jsx
@@ -0,0 +1,10 @@
+function Titulo() {
+	return (
+		<>
+			<h1>Titulo</h1>
+			<h2>subtítulo</h2>
+		</>
+	)
+}
+
+export default Titulo
---
+++ pages/basicos/usandoTitulo.jsx
@@ -0,0 +1,7 @@
+import Titulo from '../../components/Titulo'
+
+function usandoTitulo() {
+	return <Titulo />
+}
+
+export default usandoTitulo
