dfbc1e521856f706a4bc4c156633adb95507de49
Takayuki Komatsu <komatsutakayuki235@live.com>
2021-08-14 20:29:36 -0300

    integracao com css #1

--- pages/css/integracao1.jsx
+++ pages/css/integracao1.jsx
@@ -1,5 +1,11 @@
 function integracao() {
-	return <div>Enter</div>
+	return (
+		<>
+			<div className='vermelha'>Texto #1</div>
+			<div className='azul'>Texto #2</div>
+			<div className='branco'>Texto #3</div>
+		</>
+	)
 }
 
 export default integracao
